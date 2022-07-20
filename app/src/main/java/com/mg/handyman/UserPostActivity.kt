76adc5af4358ef6e30e5b3704fb71362767f73b6
Nayoung Kim <nayung228@gmail.com>
2022-07-19 22:51:50 -0700

    Created UI layouts for ChatActivity, and UserPostActivity

---
+++ app/src/main/java/com/mg/handyman/UserPostActivity.kt
@@ -0,0 +1,12 @@
+package com.mg.handyman
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_post)
+    }
+}
