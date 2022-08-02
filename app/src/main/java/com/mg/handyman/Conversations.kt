a64d27a05d9fd2e6008f7db6bcacac099dffeecb
mgill1009 <97423232+mgill1009@users.noreply.github.com>
2022-08-02 09:49:53 -0700

    - Fixed issue # 5 (Every user is able to message every other user) - Fixed issue # 15 (messaging lister if lister is not in database)

--- app/src/main/java/com/mg/handyman/Conversations.kt
+++
@@ -1,8 +0,0 @@
-package com.mg.handyman
-
-data class Conversation (
-    val messages: ArrayList<ChatMessage> = ArrayList(),
-    val createdAt: String = "",
-    val participants: ArrayList<ChatMessage> = ArrayList(),
-    val updatedAt: String = ""
-)
